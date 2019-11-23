1c50e5088df807c43e0d34481aa205ca309c0e57
league <league@imac-16.attlocal.net>
2019-11-23 11:24:35 -0800

    Backpack

---
+++ src/_01_intro_to_static/Race.java
@@ -0,0 +1,47 @@
+package _01_intro_to_static;
+
+public class Race {
+
+	
+	 static int nextBibNumber;
+	    static String raceLocation = "New York";
+	    static String raceStartTime = "9.00am";
+
+	    String name;
+	    int speed;
+	    int bibNumber;
+
+	    Race (String name, int speed){
+	        this.name = name;
+	        this.speed = speed;
+	    }
+
+	    public static void main(String[] args) {
+	        //create two athletes
+	        //print their names, bibNumbers, and the location of their race. 
+	    	String name = ("Bob");
+	    	int speed = 999999999;
+	    	int bibNumber = 1;
+	    	
+	    	String name2 = ("Bobby");
+	    	int speed2 = 1;
+	    	int bibNumber2 = 2;
+	    	
+	    	System.out.println("");
+	    	System.out.println("");
+	    	
+	    	System.out.println("Name: "+name+" Speed: "+speed+" Number: "+bibNumber);
+	    	
+	    	System.out.println("");
+	    	System.out.println("");
+	    	System.out.println("");
+	    	
+	    	System.out.println("Name: "+name2+" Speed: "+speed2+" Number: "+bibNumber2);
+	    	
+	    	System.out.println("");
+	    	System.out.println("");
+	    	
+	    }
+	
+	    
+}
--- src/_02_backpack/BackpackRunner.java
+++ src/_02_backpack/BackpackRunner.java
@@ -3,5 +3,12 @@ package _02_backpack;
 public class BackpackRunner {
 	public static void main(String[] args) {
 		new Backpack().packAndCheck();
+		
+		Backpack A = new Backpack();
+		
+		A.putInBackpack(supply);
+		A.putInBackpack(supply);
+		A.putInBackpack(supply);
+	
 	}
 }
